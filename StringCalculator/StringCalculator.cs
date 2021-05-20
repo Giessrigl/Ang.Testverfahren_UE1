2885acf2598b0a7d9a7be634c3fbfb12977c3bef
Giessrigl <christian.giessrigl@gmail.com>
2021-05-20 22:53:07 +0200

    new test cases successful

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -6,9 +6,23 @@ namespace StringCalculator
     {
         public static int Add(string numbers)
         {
-            var numberstring = numbers.Replace('\n', ',');
+            var delimiter = ',';
 
-            var stringarray = numberstring.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            var options = numbers.Split('\n', 2);
+            if (options[0].StartsWith("//"))
+            {
+                delimiter = options[0][2];
+                return Addnumbers(options[1], delimiter);
+            }
+
+            return Addnumbers(numbers, delimiter);
+        }
+
+        private static int Addnumbers(string numbers, char delimiter)
+        {
+            var numberstring = numbers.Trim().Replace('\n', delimiter);
+
+            var stringarray = numberstring.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
             var sum = 0;
             for (int i = 0; i < stringarray.Length; i++)
             {
